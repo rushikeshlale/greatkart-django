74bab43a79183bf06efafb9b89ecdcd22cc1bd39
rushikeshlale <rushikeshlale1@gmail.com>
2026-03-03 21:34:36 +0530

    Clear EMAIL_HOST_PASSWORD in settings.py
    Remove sensitive email password from settings.

--- gretcart/settings.py
+++ gretcart/settings.py
@@ -142,5 +142,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'stmp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rushikeshlale@1gmail.com'
-EMAIL_HOST_PASSWORD ='Lale@2885'
+EMAIL_HOST_PASSWORD =''
 EMAIL_USE_TLS = True
